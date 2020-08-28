9aa4f406b899e592138e64c4afc1f5baf0c676ea
rbizzell44 <ghostdogrb45@gmail.com>
2020-08-28 14:53:25 -0400

    testing

--- dev/create-instance.tf
+++ dev/create-instance.tf
@@ -2,6 +2,7 @@ resource  "google_compute_instance" "aspen-instance" {
   name = "aspen-test"
   machine_type = "f1-micro"
   zone = "us-east4-c"
+  project = "cuit-terraform-project"
 
   boot_disk {
     initialize_params {
