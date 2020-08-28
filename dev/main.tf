8e18d21869572fd3691da1fb9f6904a09db39b39
rbizzell44 <ghostdogrb45@gmail.com>
2020-08-28 17:21:11 -0400

    testing

--- dev/main.tf
+++ dev/main.tf
@@ -1,27 +1,28 @@
-resource  "google_compute_instance" "default" {
-  name         = "aspen-test"
-  machine_type = "f1-micro"
-  zone         = "us-east4-c"
+resource "google_compute_instance" "default" {
+  name = "test"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-a"
 
-  tags = ["aspen-test"]
+  tags = [
+    "foo",
+    "bar"]
 
   boot_disk {
     initialize_params {
-      image = "windows-cloud/windows-2019"
+      image = "debian-cloud/debian-9"
     }
   }
-
 
   // Local SSD disk
   scratch_disk {
     interface = "SCSI"
   }
 
-
   network_interface {
+    network = "default"
 
-    network = "aspen-vpc"
-
+    access_config {
+      // Ephemeral IP
+    }
   }
-
 }
