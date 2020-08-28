f55912aef16288bf758f45c365918a1585133cf4
rbizzell44 <ghostdogrb45@gmail.com>
2020-08-28 16:46:53 -0400

    testing

--- dev/main.tf
+++ dev/main.tf
@@ -1,31 +1,18 @@
-
- module "gci_test" {
-  source = "github.com/matti/terraform-google-compute-instance"
-
-  amount       = 3
-  region       = "us-east1"
-  name         = "test"
+resource  "google_compute_instance" "default" {
+  name = "aspen-test"
   machine_type = "f1-micro"
-  disk_size    = "32"
-  disk_image   = "windows-cloud/windows-2019"
+  zone = "us-east1-c"
 
- }
+  boot_disk {
+    initialize_params {
+      image = "windows-cloud/windows-2019"
+    }
+  }
 
-//resource  "google_compute_instance" "default" {
-//  name = "aspen-test"
-//  machine_type = "f1-micro"
- // zone = "us-east1-c"
+  network_interface {
 
- // boot_disk {
-  //  initialize_params {
-   //   image = "windows-cloud/windows-2019"
-   // }
-  //}
+    network = "aspen-vpc"
 
- // network_interface {
+  }
 
- //   network = "aspen-vpc"
-
-
-
-
+}
