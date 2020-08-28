fa212e59ee22a3a60a3439ab338f18e30513bfcf
rbizzell44 <ghostdogrb45@gmail.com>
2020-08-28 17:02:50 -0400

    testing

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,7 @@
 resource  "google_compute_instance" "default" {
   name = "aspen-test"
   machine_type = "f1-micro"
-  zone = "us-east1-c"
+  zone = "us-east4-c"
 
   boot_disk {
     initialize_params {
@@ -11,7 +11,7 @@ resource  "google_compute_instance" "default" {
 
   network_interface {
 
-    network = "gogole_computer_network.vpc_network.aspen-vpc"
+    network = "gogole_compute_network.vpc_network.aspen-vpc"
 
   }
 
