13187778eda1e8b90a8b8c30f9bf61d50b69295e
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-01 15:49:49 -0400

    test

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name = "test"
+  name = "aspen-test"
   machine_type = "n1-standard-1"
   zone = "us-central1-a"
   project = "cuit-terraform-project"
