e2d738a4e284d5bd776c4d48034ed74ac91a7236
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-04 12:57:31 -0400

    testing

--- modules/storagebucket/main.tf
+++ modules/storagebucket/main.tf
@@ -1,12 +1,12 @@
 // Create a GCS bucket
 
 resource "google_storage_bucket" "gcs-bucket" {
-  name          = "${var.bucket_name}"
-  location      = "${var.location}"
-  storage_class = "${var.storage_class}"
-  project       = "${var.project_id}"
-  force_destroy = "${var.force_destroy_property}"
-  labels        = "${var.labels}"
+  name          = var.bucket_name
+  location      = var.location
+  storage_class = var.storage_class
+  project       = var.project_id
+  force_destroy = var.force_destroy_property
+  labels        = var.labels
 
   versioning {
     enabled = true
@@ -17,8 +17,8 @@ output "bucket_name" {
   value = "${google_storage_bucket.gcs-bucket.name}"
 }
 
-resource "google_storage_bucket_iam_binding" "view_policy" {
-  bucket  = "${google_storage_bucket.gcs-bucket.name}"
-  role    = "${var.role}"
-  members = ["serviceAccount:${var.sa_email}"]
-}
+//resource "google_storage_bucket_iam_binding" "view_policy" {
+//  bucket  = "${google_storage_bucket.gcs-bucket.name}"
+//  role    = "${var.role}"
+//  members = ["serviceAccount:${var.sa_email}"]
+//}
