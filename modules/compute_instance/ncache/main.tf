3135efe9cce5bfd72deecb4f7c7215085e264f75
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-04 13:26:41 -0400

    testing

--- modules/compute_instance/ncache/main.tf
+++ modules/compute_instance/ncache/main.tf
@@ -3,32 +3,33 @@
 //
 
 resource "google_compute_instance" "default" {
-  project      = "${var.project_id}"
+  project      = var.project_id
   name         = "${var.env_initials}${var.app_initials}${var.stack_initials}"
-  zone         = "${var.region}-${var.zone}"
-  machine_type = "${var.machine_type}"
-  tags         = "${var.target_tags}"
-  labels       = "${var.labels}"
+  zone         = var.zone
+  region       = var.region
+  machine_type = var.machine_type
+  tags         = var.target_tags
+  labels       = var.labels
   allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "${var.compute_image}"
-      size  = "${var.boot_disk_size}"
-      type = "${var.disk_type}"
+      image = var.compute_image
+      size  = var.boot_disk_size
+      type =  var.disk_type
     }
   }
 
   network_interface {
-    subnetwork         = "${var.subnetwork}"
-    subnetwork_project = "${var.subnetwork_project}"
-    network_ip = "${var.ip_address}"
+    subnetwork         = var.subnetwork
+    subnetwork_project = var.subnetwork_project
+    network_ip = var.ip_address
   }
 
   metadata = var.metadata
 
   service_account {
-    email  = "${var.service_account_email}"
-    scopes = "${var.api_scopes}"
+    email  = var.service_account_email
+    scopes = var.api_scopes
   }
 }
