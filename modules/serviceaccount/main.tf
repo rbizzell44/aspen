3b937d04ccb595b4e314b8b8f02822ad7ad7d0a1
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-04 12:52:08 -0400

    testing

--- modules/serviceaccount/main.tf
+++ modules/serviceaccount/main.tf
@@ -1,9 +1,9 @@
 // Create a GCP Service Account
 
 resource "google_service_account" "service_account" {
-  project      = "${var.project_id}"
-  display_name = "${var.sa_display_name}"
-  account_id   = "${var.account_id}"
+  project      = var.project_id
+  display_name = var.sa_display_name
+  account_id   = var.account_id
 }
 
 // Uncomment below to private key for the Service Account
@@ -15,10 +15,10 @@ resource "google_service_account" "service_account" {
 
 // Outputs
 
-output "service_account_email" {
-  value = "${google_service_account.service_account.email}"
-}
+//output "service_account_email" {
+ // value = "${google_service_account.service_account.email}"
+//}
 
-output "service_account_name" {
-  value = "${google_service_account.service_account.name}"
-}
+//output "service_account_name" {
+ // value = "${google_service_account.service_account.name}"
+//}
