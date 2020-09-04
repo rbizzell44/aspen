3aca63a045befd9333a7cfcab4a3b8f58f212a45
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-04 16:10:25 -0400

    testing

--- modules/serviceaccount/main.tf
+++ modules/serviceaccount/main.tf
@@ -15,10 +15,10 @@ resource "google_service_account" "service_account" {
 
 // Outputs
 
-//output "service_account_email" {
- // value = "${google_service_account.service_account.email}"
-//}
+output "service_account_email" {
+  value = "${google_service_account.service_account.email}"
+}
 
-//output "service_account_name" {
- // value = "${google_service_account.service_account.name}"
-//}
+output "service_account_name" {
+  value = "${google_service_account.service_account.name}"
+}
