17d2a7bd772e208363a5a94131ae86e37fcebe3d
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-05 15:21:49 -0400

    testing

--- dev/main-lb-core-api.tf
+++ dev/main-lb-core-api.tf
@@ -26,43 +26,6 @@ module "gce-lb-http" {
 
 
 
- // health_check = {
- //       check_interval_sec  = null
- //       timeout_sec         = null
- //       healthy_threshold   = null
- //       unhealthy_threshold = null
- //       request_path        = "/"
- //       port                = var.service_port
- //       host                = null
- //       logging             = null
-  //}
-
-  //    log_config = {
-  //      enable = true
-  //      sample_rate = 1.0
-  //    }
-
-  //    groups = [
-        {
- //         # Each node pool instance group should be added to the backend.
- //         group                        = var.backend
- //         balancing_mode               = null
- //         capacity_scaler              = null
- //         description                  = null
- //         max_connections              = null//        max_connections_per_instance = null
- //         max_connections_per_endpoint = null
- //         max_rate                     = null
- //         max_rate_per_instance        = null
- //         max_rate_per_endpoint        = null
- //         max_utilization              = null
- //       },
- //     ]
-
- //     iap_config = {
- //       enable               = false
- //       oauth2_client_id     = null
- //         oauth2_client_secret = null
- //     }
 
 
 
