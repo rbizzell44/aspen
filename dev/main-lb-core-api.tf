94f47a21d42da0099a96ba6c6912867778eb9330
rbizzell44 <ghostdogrb45@gmail.com>
2020-09-05 15:11:43 -0400

    testing

--- dev/main-lb-core-api.tf
+++ dev/main-lb-core-api.tf
@@ -1,7 +1,12 @@
 //Load Balancer - Core API
 
-###########################
-####Backend#######
+terraform {
+  backend "gcs" {
+    bucket      = "cuit-terraform-project"
+    prefix      = "terraform/state/resources/epms-nonprod-dev-apptier"
+    //    credentials = "../../../../deployment/credentials_key.json"
+  }
+}
 
 
 
